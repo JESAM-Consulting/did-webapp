0bd8edc89f4fccf5f2db0d28b434862bebe1d6e5
Rion Kelmendi <r.kelmendi@jacobs-university.de>
2023-08-03 10:17:31 +0200

    added the rectangle behind the text

--- src/routes/home/heroBanner/index.js
+++ src/routes/home/heroBanner/index.js
@@ -19,30 +19,64 @@ export default function HeroBanner() {
               <br></br>
             </div>
             <div className="grid">
-              <div className="grid-items">
+              <div className="grid-items content-container">
                 <p>
-                Erreichen Sie Ihr Ziel mit unserem ABS-System. Unser ABS-System ist ein strukturiertes Vorgehen zur 
-                Zielerreichung: <br></br><br></br>
-                <span className="colored-word" style={{color: '#2F6EBA'}}>Analyse:</span> 
-                Erfassen der Ausgangssituation, Ihrer Ziele und relevanten Faktoren.<br></br><br></br>
-                <span className="colored-word" style={{color: '#7EAB55'}}>Beratung:</span> 
-                Entwickeln geeigneter Lösungen unter Einbeziehung Ihrer Mitarbeit. <br></br><br></br>
-                <span className="colored-word" style={{ color: '#68349A'}}>Strategie:</span> 
-                Erarbeiten eines klaren, realistischen und anpassungsfähigen Plans. <br></br><br></br>
-                Durch eine enge Zusammenarbeit erzielen wir optimale Ergebnisse. Vereinbaren Sie einen Termin und erleben <br></br>
-                Sie die Funktionsweise.
+                  Erreichen Sie Ihr Ziel mit unserem {""}
+                  <span className="colored-word" style={{ color: "#2F6EBA" }}>
+                    A
+                  </span>
+                  <span className="colored-word" style={{ color: "#5E952B" }}>
+                    B
+                  </span>
+                  <span className="colored-word" style={{ color: "#68349A" }}>
+                    S
+                  </span>
+                  -System. Unser {""}
+                  <span className="colored-word" style={{ color: "#2F6EBA" }}>
+                    A
+                  </span>
+                  <span className="colored-word" style={{ color: "#5E952B" }}>
+                    B
+                  </span>
+                  <span className="colored-word" style={{ color: "#68349A" }}>
+                    S
+                  </span>
+                  -System ist ein strukturiertes Vorgehen zur Zielerreichung:{" "}
+                  <br></br>
+                  <br></br>
+                  <span className="colored-word" style={{ color: "#2F6EBA" }}>
+                    Analyse:
+                  </span>
+                  Erfassen der Ausgangssituation, Ihrer Ziele und relevanten
+                  Faktoren.<br></br>
+                  <br></br>
+                  <span className="colored-word" style={{ color: "#5E952B" }}>
+                    Beratung:
+                  </span>
+                  Entwickeln geeigneter Lösungen unter Einbeziehung Ihrer
+                  Mitarbeit. <br></br>
+                  <br></br>
+                  <span className="colored-word" style={{ color: "#68349A" }}>
+                    Strategie:
+                  </span>
+                  Erarbeiten eines klaren, realistischen und anpassungsfähigen
+                  Plans. <br></br>
+                  <br></br>
+                  Durch eine enge Zusammenarbeit erzielen wir optimale
+                  Ergebnisse. Vereinbaren Sie einen Termin und erleben Sie die
+                  Funktionsweise.
                 </p>
               </div>
               {/* <div className="grid-items">
                 <div className="location-right-alignment">
                   <img src={LocationIcon} alt="LocationIcon" />
                 </div> */}
-                {/* <div className="main-button">
+              {/* <div className="main-button">
                   <NavLink to="/steper">
                     <button>Immobilie bewerten lassen</button>
                   </NavLink>
                 </div> */}
-                {/* <div className="button-grid">
+              {/* <div className="button-grid">
                   <div className="button-grid-items">
                     <NavLink to="/verkaufen">
                       <button>VERKAUFEN</button>
